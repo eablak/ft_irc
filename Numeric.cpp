c22bf9e576f89a4958ef303b125bcac01b59b3a5
mmesum <kaanmesum@gmail.com>
2023-08-19 15:06:32 +0300

    add

--- Numeric.cpp
+++
@@ -1,7 +0,0 @@
-#include "includes/Numeric.hpp"
-#include "includes/Server.hpp"
-
-void Numeric::printNumeric(Client &client, Server &server,std::string errMessage)
-{
-    server.messageToClient(client.getClientFd(),errMessage);
-}
--- error.cpp
+++
@@ -1,6 +0,0 @@
-#include "includes/error.hpp"
-
-void error::error_func(std::string error_msg){
-    std::cerr << error_msg << std::endl; 
-    exit(1); 
-}
