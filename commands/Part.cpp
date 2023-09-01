fd6cec223d32c83ca9274ab24685313266a5be34
EsraAblak <esrablk9@gmail.com>
2023-09-01 06:40:26 -0400

    part_op

--- commands/Part.cpp
+++ commands/Part.cpp
@@ -1,4 +1,6 @@
 #include "../includes/Part.hpp"
+#include <ctime>
+
 Part::Part()
 {
 }
@@ -23,6 +25,36 @@ void Part::handleWithParams(Server &server, Client *client, std::vector<std::str
     {
         Channel &ch = server.getChannel(params[0]);
         ch.sendMessageToChannel(server,client, "PART " + ch.getName() + " :" + params[1]);
+
+        std::cout << "ayrilan  client: " << client->getNickname() << std::endl;
+        std::vector<Client *>::iterator it;
+        int is_op = 0;
+        int any_other_op = 0;
+        for(it = ch.getOperators().begin(); it != ch.getOperators().end(); it++){
+            if ((*it)->getNickname() == client->getNickname()){
+                std::cout << "ayrilan kisi op" << std::endl;
+                is_op = 1;
+            }
+            if ((*it)->getNickname() != client->getNickname()){
+                std::cout << "baska op var " << (*it)->getNickname() << std::endl;
+                any_other_op = 1;
+            }
+        }
+        Client *new_op;
+        if (is_op == 1 && any_other_op == 0 && ch.getClients().size() != 1){
+            std::cout << "ayrilan kisi op ve baska op yok" << std::endl;
+            std::srand(std::time(0));
+            size_t rand_op = std::rand() % ch.getClients().size() + 1;
+            for(size_t i = 0; i < ch.getClients().size(); i++){
+                if (i == rand_op){
+                    new_op = ch.getClients()[i];
+                    std::cout << " new_op " << ch.getClients()[i]->getNickname() << std::endl;
+                }
+            }
+            ch.addOperator(new_op);
+        }
+
+
         ch.removeClient(client);
         client->removeChannel(ch);
     }
